d2a13c1fe88d6d89a731c68af8854c2935eb99af
Andreu729 <andresesparzag@gmail.com>
2024-06-11 11:41:55 -0400

    doesn't work still

--- class.py
+++ class.py
@@ -5,6 +5,27 @@ def list_len(list_):  # length of list
         i += 1
     return i
 
+
+def false_finder(value):
+    if not value:
+        print("Matrix error, not all rows are equal length")
+        return True
+    else:
+        return False
+
+
+false_finder([True, True, True, True])
+
+def row_len(list_):  # length calculator used for rows
+    all_lengths = []  # stores information of all row's lengths
+    standard_len = list_len(list_[0])  # row length that every row should have
+    for row in list_:
+        equals = (list_len(row) == standard_len)
+        all_lengths.append(equals)
+    for b in all_lengths:
+        t =
+
+
 a = [[2, 3, 4], [4, 5, 6]]
 print(list_len(a))
 
