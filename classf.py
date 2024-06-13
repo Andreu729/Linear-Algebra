e208c5142fe53d588969efd91d5336d6aaaf38e0
Andreu729 <andresesparzag@gmail.com>
2024-06-13 12:17:40 -0400

    matrix form and printer done

--- classf.py
+++ classf.py
@@ -65,11 +65,36 @@ def visual_constructor(matrix, n):
     k = max_finder(matrix.matrix, n)
     for row in matrix.matrix:
         m = len(str(row[n - 1]))
-        value = adder_looper(start, " ", k - m)
+        value = adder_looper(start, " ", k - m + 1)
         visual = value + str(row[n - 1])
-        visual = control_add(visual, "]", final)
+        visual = control_add(visual, " ]", final)
         constructed.append(visual)
     return constructed
+
+
+def list_union(list1, list2):
+    l1 = list_len(list1)
+    l2 = list_len(list2)
+    union_list = []
+    if not l1 == l2:
+        return union_list
+    for n in range(l1):
+        string = list1[n] + list2[n]
+        union_list.append(string)
+    return union_list
+
+
+def visual_final(matrix):
+    print_list = visual_constructor(matrix, 1)
+    for n in range(2, matrix.col_amount + 1):
+        construction = visual_constructor(matrix, n)
+        print_list = list_union(print_list, construction)
+    return print_list
+
+
+def list_print(list_):
+    for value in list_:
+        print(value)
 
 
 class Matrix:
@@ -78,3 +103,8 @@ class Matrix:
         self.matrix = matrix
         self.row_amount = list_len(matrix)
         self.col_amount = col_len(matrix)
+        self.matrix_form = visual_final(self)
+
+    def mprint(self):
+        matrix_list = self.matrix_form
+        list_print(matrix_list)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from classf import Matrix, max_finder
+from classf import *
 imatrix = Matrix([[1, 0], [0, 1], [1, 0], [4, 5]])
 print(imatrix.matrix)
 print(imatrix.row_amount)
 print(imatrix.col_amount)
 print("=================")
-a = [[111, 0], [0, 1], [-178, 0], [4, 5]]
-b = max_finder(a, 1)
-print(b)
+xd = imatrix.matrix_form
+imatrix.mprint()
