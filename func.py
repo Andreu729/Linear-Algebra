b4ac134b07d4690bc9ef38664e65a132e5536df6
Andreu729 <andresesparzag@gmail.com>
2024-06-15 11:02:13 -0400

    operations for matrix rows

--- func.py
+++ func.py
@@ -15,3 +15,39 @@ def column_extend(matrix, b):  # extends the og matrix introducing the new b col
         new_matrix.append(row)
         i += 1
     return Matrix(new_matrix)
+
+
+def single_reduce(matrix, n):  # reduces one single column of matrix and returns it as the same.
+    pass
+
+
+def row_amplification(matrix, row, n):  # multiplies the row of the matrix by an n factor
+    matrix = matrix.matrix
+    new_row = []
+    for value in matrix[row - 1]:
+        value = value * n
+        new_row.append(value)
+    matrix[row - 1] = new_row
+    return Matrix(matrix)
+
+
+def row_permutation(matrix, row_1, row_2):  # changes positions between row_1 and row_2
+    matrix = matrix.matrix
+    first_row = matrix[row_1 - 1]
+    second_row = matrix[row_2 - 1]
+    matrix[row_1 - 1] = second_row
+    matrix[row_2 - 1] = first_row
+    return Matrix(matrix)
+
+
+def row_operation(matrix, row_1, row_2, n):  # row operation, row_1 -> row_1 + n * row_2
+    matrix_ = matrix.matrix
+    number = row_1
+    row_1 = matrix_[row_1 - 1]
+    row_2 = matrix_[row_2 - 1]
+    new_row = []
+    for i in range(matrix.col_amount):
+        value = row_1[i] + row_2[i] * n
+        new_row.append(value)
+    matrix_[number - 1] = new_row
+    return Matrix(matrix_)
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from func import *
 
 imatrix = Matrix([[1, 0, 5, 5], [0, 1, 3, -3], [1, 0, 1670, -47]])
-print(imatrix.matrix)
-print(imatrix.row_amount)
-print(imatrix.col_amount)
-print("=================")
-xd = imatrix.matrix_form
 imatrix.mprint()
-n_matrix = column_extend(imatrix, [0, -1, 2])
+print("=====================")
+n_matrix = row_operation(imatrix, 3, 1, -1)
 n_matrix.mprint()
